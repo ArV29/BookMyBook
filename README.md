325f77f6be5d1e66530ecd5c3ad465cac208db32
ArV <aryanrajvaidya@gmail.com>
2021-11-27 22:57:46 +0530

    deleting all

--- README.md
+++
@@ -1,3 +0,0 @@
-# book_my_book
-
-Sell and Buy Used Books
--- android/app/src/main/kotlin/in/bmb/book_my_book/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package `in`.bmb.book_my_book
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
