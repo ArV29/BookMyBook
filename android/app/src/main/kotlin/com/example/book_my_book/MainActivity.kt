92b68f1acb30a214be6c3239349833feb072a19f
ArV <aryanrajvaidya@gmail.com>
2021-12-04 22:07:24 +0530

    cleaning slate

--- android/app/src/main/kotlin/com/example/book_my_book/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.book_my_book
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/constants.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-Color kTransparentBlue = Color(0xFFE3F0FF);
-Color kYellow = Color(0xFFFD8600);
-Color kTextGray = Color(0xFF5B5B5B);
